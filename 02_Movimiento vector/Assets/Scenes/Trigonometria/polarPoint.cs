3ca9f591cc801c7efc31063345e81a98819ad3c9
Lunaysa <anamaria271127@gmail.com>
2022-03-10 10:42:26 -0500

    Trigonometria

---
+++ 02_Movimiento vector/Assets/Scenes/Trigonometria/polarPoint.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class polarPoint : MonoBehaviour
+{
+    [SerializeField] float radio;
+    [SerializeField] float tetha;
+
+
+    [Header("Movement speed")]
+
+    [SerializeField] private float radialSpeed = 1f;
+    [SerializeField] private float angularSpeed = 1f;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tetha += angularSpeed * Time.deltaTime;
+        radio += radialSpeed * Time.deltaTime;
+
+        Vector3 cartesian = polarToCartesian(radio, tetha);
+        transform.localPosition = cartesian;
+
+        Debug.DrawLine(Vector3.zero, cartesian);
+
+        if (radio >= 5 || radio <= 5)
+        {
+            radio -= radialSpeed * Time.deltaTime;
+        }
+
+    }
+
+    Vector3 polarToCartesian(float radio, float tetha)
+    {
+        return new Vector3(radio * Mathf.Cos(tetha), radio * Mathf.Sin(tetha));
+    }
+}
